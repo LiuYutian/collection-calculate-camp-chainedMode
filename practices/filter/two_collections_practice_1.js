aec4d9a7b898a8d4c7e22e6875ce7188223872f1
LiuYutian <l709178159@gmail.com>
2015-06-11 23:53:41 +0800

    two_coolection_practice_1 with intersection function in prototype chain

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -1,9 +1,8 @@
 'use strict';
 
+var _ = require('../../myLodash.js');
 function choose_common_elements(collection_a, collection_b) {
-    var _ = require('../../myLodash.js');
-
-    return _.intersection(collection_a, collection_b);
+    return _(collection_a).intersection(collection_b).value();
 }
 
 module.exports = choose_common_elements;
