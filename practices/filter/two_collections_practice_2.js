fcb46b2455f5294ee8ccb5b74d69fd7cf00eb73b
LiuYutian <l709178159@gmail.com>
2015-06-12 00:11:40 +0800

    two_collections_practice_2 with each and exist function

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -1,18 +1,18 @@
 'use strict';
 
+var _ = require('../../myLodash.js');
 function choose_no_common_elements(collection_a, collection_b) {
-    var _ = require('../../myLodash.js');
     var result = [];
 
-    _.each(collection_a,function (letter) {
-        if (!_.exist(collection_b,letter)) {
-            result[result.length] = letter;
+    _(collection_a).each(function (n) {
+        if (!_(collection_b).exist(n)) {
+            result.push(n);
         }
     });
 
-    _.each(collection_b,function (letter) {
-        if (!_.exist(collection_a,letter)) {
-            result[result.length] = letter;
+    _(collection_b).each(function (n) {
+        if (!_(collection_a).exist(n)) {
+            result.push(n);
         }
     });
 
