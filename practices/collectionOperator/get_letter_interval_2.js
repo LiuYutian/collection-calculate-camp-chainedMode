914fb90df46c53510c775802695416d3b2040632
LiuYutian <l709178159@gmail.com>
2015-06-11 15:25:13 +0800

    get_letter_interval_2 with range and map function in prototype chain

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -2,25 +2,6 @@
 
 var _ = require('../../myLodash.js');
 
-function getChar(number) {
-    if(number>26) {
-        var carry = parseInt(number/26);
-        if(number%26 === 0) {
-            return String.fromCharCode(carry-1+96)+String.fromCharCode(26+96);
-        }
-        else {
-            return String.fromCharCode(carry+96)+String.fromCharCode(number%26+96);
-        }
-    }else {
-        if(number%26 === 0) {
-            return String.fromCharCode(26+96);
-        }
-        else {
-            return String.fromCharCode(number%26+96);
-        }
-    }
-}
-
 function get_letter_interval_2(number_a, number_b) {
     return _(number_a).range(number_b).map(function(n){
         return _().getChar(n);
