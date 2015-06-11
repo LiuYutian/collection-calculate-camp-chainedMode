30b08bed701e38c056570f083556fc77be485b21
LiuYutian <l709178159@gmail.com>
2015-06-12 00:54:58 +0800

    number_map_to_word_over_26 with map and getChar function in prototpye chain

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,29 +1,10 @@
 'use strict';
+
+var _ = require('../../myLodash.js');
 var number_map_to_word_over_26 = function(collection){
-    var _ = require('../../myLodash.js');
-
-    return _.map(collection, function(n){
-        return MapChar(n);
-    });
-};
-
-function MapChar(n) {
-    if(n>26) {
-        var carry = parseInt(n/26);
-        if(n%26 === 0) {
-            return String.fromCharCode(carry-1+96)+String.fromCharCode(26+96);
-        }
-        else {
-            return String.fromCharCode(carry+96)+String.fromCharCode(n%26+96);
-        }
-    }else {
-        if(n%26 === 0) {
-            return String.fromCharCode(26+96);
-        }
-        else {
-            return String.fromCharCode(n%26+96);
-        }
-    }
+    return _(collection).map(function(n){
+        return _().getChar(n);
+    }).value();
 };
 
 module.exports = number_map_to_word_over_26;
