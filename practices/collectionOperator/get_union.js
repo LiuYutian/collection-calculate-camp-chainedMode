7ca73a13348cbd5b87c0d6cdae42c5857294eff2
LiuYutian <l709178159@gmail.com>
2015-06-11 15:44:08 +0800

    git_union with concat and deWeight function in prototype chain

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,18 +1,9 @@
 'use strict';
 
+var _  = require('../../myLodash.js');
 function get_union(collection_a, collection_b) {
-    var _  = require('../../myLodash.js');
-    var array = [];
 
-    _.each(collection_a, function(n){
-        array.push(n);
-    });
-
-    _.each(collection_b, function(n){
-        array.push(n);
-    });
-
-    return _.deWeight(array);
+    return _(collection_a).concat(collection_b).deWeight().value();
 }
 
 module.exports = get_union;
