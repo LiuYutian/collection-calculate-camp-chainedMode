ed3c81140ef2b5ad636019aecd44ef386e978a6c
LiuYutian <l709178159@gmail.com>
2015-06-11 14:08:05 +0800

    chainedMode myLodash change collectionOperator/get_integer_interval.js

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -3,7 +3,7 @@
 function get_integer_interval(number_a, number_b) {
     var _ = require('../../myLodash.js');
 
-    return _.range(number_a, number_b);
+    return _(number_a).range(number_b).value();
 }
 
 module.exports = get_integer_interval;
