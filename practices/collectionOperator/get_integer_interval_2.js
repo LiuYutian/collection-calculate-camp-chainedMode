e87ffb2275117ba64876570bc49b03db9544dc55
LiuYutian <l709178159@gmail.com>
2015-06-11 14:12:10 +0800

    chaineMode myLodash cheng collectionOperator/get_integer_interval_2.js

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -3,11 +3,9 @@
 function get_integer_interval_2(number_a, number_b) {
     var _ = require('../../myLodash.js');
 
-    var array = _.range(number_a, number_b);
-
-    return _.filter(array, function(n){
+    return _(number_a).range(number_b).filter(function(n){
         return n % 2 === 0;
-    });
+    }).value();
 }
 
 module.exports = get_integer_interval_2;
