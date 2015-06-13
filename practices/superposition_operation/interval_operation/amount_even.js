c1a1adecfbdd4bba0f7be394c16a62515c22e523
LiuYutian <l709178159@gmail.com>
2015-06-13 18:57:44 +0800

    amount_even.js with filter and reduce function in prototype chain

--- practices/superposition_operation/interval_operation/amount_even.js
+++ practices/superposition_operation/interval_operation/amount_even.js
@@ -1,25 +1,12 @@
 'use strict';
 
+var _ = require('../../../myLodash.js');
 function amount_even(collection) {
-    var _ = require('../../../myLodash.js');
-
-    var array = _.filter(collection, function(n) {
+    return _(collection).filter(function(n) {
         return n % 2 === 0;
-    });
-
-    return _.reduce(array, function(a, b){
+    }).reduce(function(a, b){
         return a+b;
-    });
-
-    var sum = 0;
-
-    for(var i = 0; i < collection.length; i++) {
-        if(collection[i] % 2 === 0) {
-            sum += collection[i];
-        }
-    }
-
-    return sum;
+    }).value();
 }
 
 module.exports = amount_even;
