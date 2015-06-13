9203967126e111fe822165ac7389df037b5acf53
LiuYutian <l709178159@gmail.com>
2015-06-13 20:47:39 +0800

    rank_by_two_large_one_small.js sort function

--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -3,10 +3,10 @@
 var _ = require('../../../myLodash.js');
 
 function rank_by_two_large_one_small(collection){
-    collection = _.sort(collection, function(a, b){
-        return a > b;
+    collection = _(collection).sort(function(a, b){
+        return a < b;
     });
-    
+
     for(var i = 0; i < collection.length-2; i += 3) {
         var temp = collection[i];
         collection[i] = collection[i+1];
