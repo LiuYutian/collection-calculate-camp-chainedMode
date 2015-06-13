bea37b76e8aa1bafd10e99ff79bf374abee10c66
LiuYutian <l709178159@gmail.com>
2015-06-13 20:32:25 +0800

    is_exist_element.js with filter and exist function in prototype chain

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,12 +1,9 @@
 'use strict';
 
 var _ = require('../../../../myLodash.js');
-
 var is_exist_element = function(collection,element){
-    var filterArray = _.filter(collection, function(n, i){
+    return _(collection).filter(function(n, i){
         return i % 2 === 0;
-    });
-
-    return _.exist(filterArray, element);
+    }).exist(element);
 };
 module.exports = is_exist_element;
