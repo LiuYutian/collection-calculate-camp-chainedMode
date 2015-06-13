f79a4fe89503e243eb24d5fa867bad03ee260d4c
LiuYutian <l709178159@gmail.com>
2015-06-13 10:36:13 +0800

    collection_max_number.js with reduce and max function in prototype chain

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -3,9 +3,9 @@
 function collect_max_number(collection) {
     var _ = require('../../myLodash.js');
 
-    return _.reduce(collection, function(a, b){
+    return _(collection).reduce(function(a, b){
         return Math.max(a,b);
-    });
+    }).value();
 }
 
 module.exports = collect_max_number;
