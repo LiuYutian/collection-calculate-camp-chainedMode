b4ac59d3fcdbe9c4594b9a19b4310f5b5bf1300f
LiuYutian <l709178159@gmail.com>
2015-06-13 11:01:45 +0800

    find_element_first_subscript.js with each function and exist mark

--- practices/reduce/find_element_first_subscript.js
+++ practices/reduce/find_element_first_subscript.js
@@ -1,10 +1,10 @@
 'use strict';
 
+var _ = require('../../myLodash.js');
 function calculate_elements_sum(collection, element) {
-    var _ = require('../../myLodash.js');
     var index = -1;
     var exist = true;
-    _.each(collection, function(n , i){
+    _(collection).each(function(n , i){
         if(n===element && exist){
             index = i;
             exist = false;
