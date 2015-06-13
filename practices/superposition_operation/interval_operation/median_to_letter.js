0783ba852a4257ee4e76c4380f2cc70804a3dda2
LiuYutian <l709178159@gmail.com>
2015-06-13 19:05:34 +0800

    median_to_letter.js with getChar ceil and median function in prototype chain

--- practices/superposition_operation/interval_operation/median_to_letter.js
+++ practices/superposition_operation/interval_operation/median_to_letter.js
@@ -1,28 +1,8 @@
 'use strict';
 
 var _ = require('../../../myLodash.js');
-
 function median_to_letter(collection) {
-    return MapChar(Math.ceil(_.median(collection)));
-}
-
-function MapChar(n) {
-    if(n>26) {
-        var carry = parseInt(n/26);
-        if(n%26 === 0) {
-            return String.fromCharCode(carry-1+96)+String.fromCharCode(26+96);
-        }
-        else {
-            return String.fromCharCode(carry+96)+String.fromCharCode(n%26+96);
-        }
-    }else {
-        if(n%26 === 0) {
-            return String.fromCharCode(26+96);
-        }
-        else {
-            return String.fromCharCode(n%26+96);
-        }
-    }
+    return _().getChar(Math.ceil(_(collection).median()));
 }
 
 module.exports = median_to_letter;
