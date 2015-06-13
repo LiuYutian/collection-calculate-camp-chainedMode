b9fac29b14ff93ca4ce78ee06f47fdc6020344f2
LiuYutian <l709178159@gmail.com>
2015-06-13 18:52:13 +0800

    practice_2.js with filter and map function in prototype chain

--- practices/superposition_operation/collection_operation/practice_2.js
+++ practices/superposition_operation/collection_operation/practice_2.js
@@ -1,15 +1,12 @@
 'use strict';
 
+var _ = require('../../../myLodash.js');
 function hybrid_operation_to_uneven(collection) {
-    var _ = require('../../../myLodash.js');
-
-    var array = _.filter(collection, function(n){
+    return _(collection).filter(function(n){
         return n % 2 != 0;
-    });
-
-    return _.map(array, function(n){
+    }).map(function(n){
         return n * 3 + 2;
-    });
+    }).value();
 }
 
 module.exports = hybrid_operation_to_uneven;
