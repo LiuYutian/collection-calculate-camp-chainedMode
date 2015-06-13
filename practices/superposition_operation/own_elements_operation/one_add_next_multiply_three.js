3bed2925b78b4c8b42ef9d8eb4772142a5dce604
LiuYutian <l709178159@gmail.com>
2015-06-13 20:46:06 +0800

    one_add_next_multiply_three.js each function

--- practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -5,7 +5,7 @@ var _ = require('../../../myLodash.js');
 function one_add_next_multiply_three(collection){
     var resultArray = [];
 
-    _.each(collection, function(n, i){
+    _(collection).each(function(n, i){
         if(i < collection.length-1){
             resultArray.push((n + collection[i + 1]) * 3);
         }
