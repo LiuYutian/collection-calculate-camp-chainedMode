d2d461d6b6322ba07fc9ff9d9b94d7392bf60d28
LiuYutian <l709178159@gmail.com>
2015-06-13 18:48:02 +0800

    letter_mapping_2.js with getChar ceil reduce and value function in prototype chain

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,13 +1,10 @@
 'use strict';
 
+var _ = require('../../../myLodash.js');
 function average_to_letter(collection) {
-    var _ = require('../../../myLodash.js');
-
-    var sum = _.reduce(collection, function(a, b){
+    return _().getChar(Math.ceil(_(collection).reduce(function(a, b){
         return a + b;
-    });
-
-    return String.fromCharCode(Math.ceil(sum/collection.length)+96);
+    }).value()/collection.length));
 }
 
 module.exports = average_to_letter;
